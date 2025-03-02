56475aa8f0b3f926be3042f9846c618027a5c922
lonshanworld <lonshan3010@gmail.com>
2025-03-02 18:12:19 +0700

    finish

--- csvProject/urls.py
+++ csvProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('challenges.urls')),  # Include the challenges app URLs
+    path('', include('challenges.urls')), 
 ]
